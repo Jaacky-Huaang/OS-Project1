8c3d5a3657dabdbf822b6f36a80d4971f80cf48d
Lihan_Feng <lihanfeng2022@gmail.com>
2023-09-25 10:24:25 +0400

    create shell_framework.c

--- shell_framework.c
+++ shell_framework.c
@@ -5,3 +5,28 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+int main() {
+    char input[256];
+    printf("MyShell session started...\n");
+    while (1) {
+        // ask for a prompt
+        printf("Enter Shell Command:\n>");
+        scanf(" %[^\n]", input);
+        // Exit the shell if the user enters "exit" or "quit"
+        if (strcmp(input, "exit") == 0 || strcmp(input, "quit") == 0) {
+            printf("Exiting MyShell...\n");
+            break;
+        }
+
+        // Execute the user's command
+        execute_command(input);
+    }
+
+    return 0;
+}
+
+
+
+
+
+
