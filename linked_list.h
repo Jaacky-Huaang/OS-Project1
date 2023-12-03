a899fec2fc7f0540f7df59e498bf1116c6368ff0
Jaacky-Huaang <104845011+Jaacky-Huaang@users.noreply.github.com>
2023-12-03 13:47:28 +0400

    Update linked_list.h

--- linked_list.h
+++ linked_list.h
@@ -4,7 +4,8 @@
 #include <pthread.h>
 
 // Define the struct for a unit in the single linked list
-typedef struct node {
+typedef struct node 
+{
     pthread_t thread_id; // Client's thread id
     int semaphore_id; // Semaphore id
     char input[1024]; // The message the client input
@@ -13,16 +14,21 @@ typedef struct node {
     // The program's time will decrement by 1 every second
     int round; // The round of the client
     int runned_time; // The runned time of the client
+    int total_time;
+    int total_progress;
     struct node *next;
+    int socket;
+    int client_id;
 } node;
 
 // Function prototypes
-node* create_node(pthread_t thread_id, int semaphore_id, char input[1024], int remaining_time,int round,int runned_time);
+node* create_node(pthread_t thread_id, int semaphore_id, char input[1024], int remaining_time,int round,int runned_time,int socket);
 void tail_insert(node **head, node *new_node);
 void head_insert(node **head, node *new_node);
 void delete_node(node **head, pthread_t thread_id);
 node* search_node(node *head, pthread_t thread_id);
 void print_node(const node *n);
 void print_list(const node *head);
+int get_list_length(const node *head);
 
 #endif // LINKED_LIST_H
